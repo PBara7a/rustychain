60472c229ace2faf034477677a6bcdb3450ecab2
Paulo Barata <pbara7a@gmail.com>
2022-06-02 17:06:13 +0100

    Method to validate the blockchain.

--- spec/blockchain.test.js
+++ spec/blockchain.test.js
@@ -2,7 +2,11 @@ const Blockchain = require("../src/blockchain");
 const Block = require("../src/block");
 
 describe("Blockchain", () => {
-  const blockchain = new Blockchain();
+  let blockchain;
+
+  beforeEach(() => {
+    blockchain = new Blockchain();
+  });
 
   it("has a chain array", () => {
     expect(blockchain.chain instanceof Array).toBe(true);
@@ -17,5 +21,35 @@ describe("Blockchain", () => {
     blockchain.addBlock({ data: newData });
 
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+  });
+
+  describe("isValidChain", () => {
+    beforeEach(() => {
+      blockchain.addBlock({ data: "foo" });
+      blockchain.addBlock({ data: "bar" });
+      blockchain.addBlock({ data: "baz" });
+    });
+
+    it("returns false when the genesis block is missing", () => {
+      blockchain.chain[0] = { data: "foo" };
+
+      expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+    });
+
+    it("returns false when a previousBlockHash has changed", () => {
+      blockchain.chain[2].previousBlockHash = "tampered hash";
+
+      expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+    });
+
+    it("returns false when there is a block with an invalid field", () => {
+      blockchain.chain[2].data = "tampered data";
+
+      expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+    });
+
+    it("returns true when all blocks are valid", () => {
+      expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
+    });
   });
 });
--- src/blockchain.js
+++ src/blockchain.js
@@ -1,4 +1,5 @@
 const Block = require("./block");
+const cryptographicHash = require("./utils/cryptographicHash");
 
 class Blockchain {
   constructor() {
@@ -10,6 +11,23 @@ class Blockchain {
     const newBlock = Block.mineBlock({ lastBlock, data });
 
     this.chain.push(newBlock);
+  }
+
+  static isValidChain(chain) {
+    // compared the string version of the object instead of references
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+      return false;
+
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, previousBlockHash, data, hash } = chain[i];
+      const actualPreviousBlockHash = chain[i - 1].hash;
+      const validHash = cryptographicHash(timestamp, previousBlockHash, data);
+
+      if (previousBlockHash !== actualPreviousBlockHash) return false;
+      if (hash !== validHash) return false;
+    }
+
+    return true;
   }
 }
 
